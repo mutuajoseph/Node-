e714c679e77036ae2f1a72d2960f576c4bcc1b0a
mutuajoseph <mutuajoseph17@gmail.com>
2020-09-21 19:34:45 +0300

    First commit

---
+++ FirstApp/app.js
@@ -0,0 +1,27 @@
+// function sayHello(name) {
+//     console.log('Hello' + name)
+// }
+
+// sayHello('mosh')
+
+// global objects
+
+// setTimeout()
+// clearTimeout()
+// setInterval()
+// clearInterval()
+
+// modules - every node file has one main file which is a module
+//         - Its not a global object
+
+// creating a module
+
+
+var logger = require('./logger');
+// console.log(logger);
+
+logger.log('Hello')
+
+
+
+
---
+++ FirstApp/logger.js
@@ -0,0 +1,8 @@
+var url = 'http://www.mylogger.io'
+
+function log(message) {
+    console.log(message)
+}
+
+// export the log module 
+module.exports.log = log;
